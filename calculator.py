a45a003fbd2a7ee634ffb0774f521d0eb422de2c
aziz <azizalghanim98@gmail.com>
2022-04-08 08:00:13 +0300

    task complete, test pass

--- calculator.py
+++ calculator.py
@@ -1,63 +1,50 @@
 
 def main():
 	# inputs
-	while True:
-		try:
-			num1 = int(input('Enter the first number: '))
-		except ValueError:
-			print('That won\'t work!')
-			continue
-		else:
-			break
-		
-	while True:
-		try:
-			num2 = int(input('Enter the second number: '))
-		except ValueError:
-			print('That won\'t work!')
-			continue
-		else:
-			break
-		
-	while True:
-		operation = str(input('choose your operation (+, -, /, *): '))
-		operations = ('+', '-', '/', '*')
-		if not operation in operations: 
-			print('please enter one of the following operations (+, -, /, *)')
-		else:
-			break
-
-	# https://stackoverflow.com/questions/23294658/asking-the-user-for-input-until-they-give-a-valid-response
-	
+	num1 = input('Enter the first number: ')
+	if not num1.isnumeric():
+		print('no istahbiling please, only numbers.')
+	elif num1.isnumeric():
+		num2 = (input('Enter the second number: '))
+		if not num2.isnumeric():
+			print("sorry we're still not accepting any istihbal")
+		elif num2.isnumeric():
+			operation = str(input('choose your operation (+, -, /, *): '))
+			operations = ('+', '-', '/', '*')
+			if not operation in operations:
+				print("you're istihabal fund has expired please call customer support")
+			elif operation in operations:
+				
 	# validators
+	# https://stackoverflow.com/questions/23294658/asking-the-user-for-input-until-they-give-a-valid-response
 
-	# output
-	calculation = print(f'{num1} {operation} {num2} is ')
+				# output
+				print(f'{num1} {operation} {num2} is ')
 
 
-	# operations
-	def add():
-		result = num1 + num2
-		print( result)
-	def subtract():
-		result = num1 - num2
-		print(result)
-	def devide():
-		result = num1 / num2
-		print(result)
-	def multiply():
-		result = num1 * num2
-		print(result)
+				# operations
+				def add():
+					result = int(num1) + int(num2)
+					print( result)
+				def subtract():
+					result = int(num1) - int(num2)
+					print(result)
+				def devide():
+					result = int(num1) / int(num2)
+					print(result)
+				def multiply():
+					result = int(num1) * int(num2)
+					print(result)
 
-	# conditions for performing operations
-	if operation == '+':
-		add()
-	elif operation == '-':
-		subtract()
-	elif operation == '/':
-		devide()
-	elif operation == '*':
-		multiply()
+				# conditions for performing operations
+				if operation == '+':
+					add()
+				elif operation == '-':
+					subtract()
+				elif operation == '/':
+					devide()
+				elif operation == '*':
+					multiply()
 
 if __name__ == '__main__':
 	main()
